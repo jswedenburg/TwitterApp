a092bd3ff95719f57d97351664f65c293d554fbe
jswedenburg <jswedenburg@gmail.com>
2016-10-07 14:46:45 -0600

    Added collection view to tableview by container

---
+++ TwitterApp/AccountCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AccountCollectionViewCell.swift
+//  TwitterApp
+//
+//  Created by Jake SWEDENBURG on 10/7/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import UIKit
+
+class AccountCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var accountNameLabel: UILabel!
+    
+    
+    
+}
