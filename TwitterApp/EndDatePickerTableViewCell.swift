7c22cf45feaca09545ed203545d7841a9caa0e0b
jswedenburg <jswedenburg@gmail.com>
2016-10-09 09:49:40 -0600

    Added Detail Custom Cells

---
+++ TwitterApp/EndDatePickerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EndDatePickerTableViewCell.swift
+//  TwitterApp
+//
+//  Created by Jake SWEDENBURG on 10/9/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import UIKit
+
+class EndDatePickerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TwitterApp/ScheduleDetailTableViewController.swift
+++ TwitterApp/ScheduleDetailTableViewController.swift
@@ -32,7 +32,6 @@ class ScheduleDetailTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         switch section {
         case 0:
             return 7
@@ -43,23 +42,13 @@ class ScheduleDetailTableViewController: UITableViewController {
         }
     }
     
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    
+    
+    
     
     /*
      // Override to support editing the table view.
