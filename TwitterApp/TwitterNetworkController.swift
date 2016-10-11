f99fc8f094b3609dc65890f5a34a45237ee6ce87
jswedenburg <jswedenburg@gmail.com>
2016-10-11 15:37:03 -0600

    Starting Twitter API JSON

---
+++ TwitterApp/TwitterNetworkController.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterNetworkController.swift
+//  TwitterApp
+//
+//  Created by Jake SWEDENBURG on 10/11/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import Foundation
+
+class TwitterNetworkController {
+    
+    //MARK
+}
