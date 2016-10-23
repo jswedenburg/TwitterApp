031c6fe9705e3c31fb50e069f5a747ced5e2ef15
jswedenburg <jswedenburg@gmail.com>
2016-10-23 16:06:11 -0600

    refining UI

--- ScheduleTableViewController.swift
+++ ScheduleTableViewController.swift
@@ -13,7 +13,7 @@ import UIKit
 class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, TableViewCellDelegate {
     
     
-   
+   let twitterBlue = UIColor(red: 29/255, green: 161/255, blue: 242/155, alpha: 1.0)
     
     
     var scheduleArray: [Schedule] {
@@ -29,8 +29,20 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
         }
     }
     
+    override func viewDidLoad() {
+        if let titleFont = UIFont(name: "PaulGrotesk-Bold-Trail", size: 25) {
+            self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white, NSFontAttributeName: titleFont]
+        }
+        
+        print(UIFont.fontNames(forFamilyName: "Paul Grotesk"))
+        
+        self.navigationController?.navigationBar.barTintColor = twitterBlue
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.tableView.reloadData()
+        
+        
         
         self.tableView.tableFooterView = UIView(frame: CGRect.zero)
         
@@ -71,7 +83,7 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
         guard let cell = sender as? ScheduleTableViewCell else { return }
         cell.followButton.isEnabled = false
         guard let sender = sender as? ScheduleTableViewCell else { return }
-        guard let index = self.tableView.indexPath(for: sender)?.row else { return }
+        guard let index = self.tableView.indexPath(for: sender)?.section else { return }
         let schedule = scheduleArray[index - 1]
         let accounts = schedule.twitterAccounts?.allObjects as? [TwitterAccount] ?? []
         
@@ -127,18 +139,18 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return scheduleArray.count + 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-            return scheduleArray.count + 1
+            return 1
 
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             guard let titleCell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath) as? TitleTableViewCell else { return UITableViewCell()}
             titleCell.titleLabel.text = "Groups"
             titleCell.isUserInteractionEnabled = false
@@ -149,7 +161,7 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
             return titleCell
         } else {
             guard let scheduleCell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell", for: indexPath) as? ScheduleTableViewCell else { return UITableViewCell() }
-            let schedule = scheduleArray[indexPath.row - 1]
+            let schedule = scheduleArray[indexPath.section - 1]
             let accountArray2 = schedule.twitterAccounts?.allObjects as! [TwitterAccount]
             scheduleCell.updateWithSchedule(schedule: schedule, accounts: accountArray2)
             if schedule.enabled {
@@ -162,7 +174,11 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
             
             //scheduleCell.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
             
+            scheduleCell.contentView.layer.borderColor = UIColor(red: 170/255, green: 184/255, blue: 194/255, alpha: 1.0).cgColor
             
+            scheduleCell.contentView.layer.borderWidth = 0.2
+            scheduleCell.contentView.layer.cornerRadius = 10
+            scheduleCell.layer.cornerRadius = 10
             
             return scheduleCell
         }
@@ -171,39 +187,29 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0  {
-            return 100
-        } else if indexPath.row == 1{
+        if indexPath.section == 0  {
+            return 63
+        } else if indexPath.section == 1{
             return 90
         }else {
             return 90
         }
     }
     
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row != 0 {
-            cell.contentView.backgroundColor = UIColor.clear
-            
-            let whiteRoundedView : UIView = UIView(frame: CGRect(x: 0, y: 3, width: self.view.frame.size.width, height: self.view.frame.size.height))
-            
-            whiteRoundedView.layer.backgroundColor = UIColor.white.cgColor
-            whiteRoundedView.layer.masksToBounds = false
-            whiteRoundedView.layer.cornerRadius = 2.0
-            
-            whiteRoundedView.layer.shadowOffset = CGSize(width: -1, height: -10)
-            whiteRoundedView.layer.shadowOpacity = 0.2
-            whiteRoundedView.layer.shadowColor = UIColor(red: 170/255, green: 184/255, blue: 194/255, alpha: 1).cgColor
-            
-            cell.contentView.addSubview(whiteRoundedView)
-            cell.contentView.sendSubview(toBack: whiteRoundedView)
-        }
-        
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "footer"
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "header"
+    }
+    
+    
     
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let schedule = scheduleArray[indexPath.row - 1]
+            let schedule = scheduleArray[indexPath.section - 1]
             ScheduleController.sharedController.delete(schedule)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
@@ -220,7 +226,7 @@ class ScheduleTableViewController: UIViewController, UITableViewDelegate, UITabl
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let navVC = segue.destination as? UINavigationController, let detailVC = navVC.topViewController as? ScheduleDetailViewController, let index = tableView.indexPathForSelectedRow?.row else {return}
+        guard let navVC = segue.destination as? UINavigationController, let detailVC = navVC.topViewController as? ScheduleDetailViewController, let index = tableView.indexPathForSelectedRow?.section else {return}
         
         if segue.identifier == "editSchedule"{
                 detailVC.schedule = scheduleArray[index - 1]
